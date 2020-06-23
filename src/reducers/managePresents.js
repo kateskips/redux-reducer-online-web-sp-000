62c0c9bf049d9becd64bee133002494fa3cbf4a1
Kate F <follett.katie@gmail.com>
2020-06-23 17:53:26 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,3 +1,12 @@
 export function managePresents(state = {numberOfPresents: 0}, action){
-  
+    switch(action.type) {
+
+    case "INCREASE":
+      return Object.assign({}, state, {
+        numberOfPresents: state.numberOfPresents + 1
+      });
+
+    default:
+      return state;
+  }
 }
